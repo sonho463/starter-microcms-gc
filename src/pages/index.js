05bcc9e782065bd1ae1a0c490009222037bf19c6
sonho463 <son.ho463@gmail.com>
2021-05-02 13:10:35 +0900

    push -u

--- src/pages/index.js
+++ src/pages/index.js
@@ -119,6 +119,8 @@ export default function Home() {
             </div>
           </div>
         </header>
+
+			
       </BackgroundImage>
       {/* Main Content*/}
       <div className="container">
