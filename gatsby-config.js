120e79b45bafbec083ab65f9fd1543c60d70675e
sonho463 <son.ho463@gmail.com>
2021-05-02 11:53:56 +0900

    背景画像用のプラグインインストールまで完了

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,19 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require(`path`)
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
