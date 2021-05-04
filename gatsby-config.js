a0d273973b6a48d324a17d565d89d4fbb20c7229
sonho463 <son.ho463@gmail.com>
2021-05-04 13:25:29 +0900

    リッチテキスト準備

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,26 @@ module.exports = {
     title: `ほるもんTのブログ`,
     siteUrl: "https://blog-by-microcms.netlify.app",
     description: `これは、GatsbyとmicroCMSで作ったブログです。`,
-		lang: `ja`,
+    lang: `ja`,
   },
   /* Your site config here */
   plugins: [
-		{
-      resolve: 'gatsby-source-microcms',
+    {
+      resolve: "gatsby-source-microcms",
       options: {
         apiKey: process.env.MICROCMS_API_KEY,
         serviceId: process.env.MICROCMS_SERVICE_ID,
-        apis: [{
-          endpoint: 'posts',
-        }],
+        apis: [
+          {
+            endpoint: "posts",
+          },
+					{
+						endpoint: "category",
+					},
+					{
+						endpoint: "tag",
+					},
+        ],
       },
     },
     {
@@ -33,6 +41,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-		`gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,13 +1,17 @@
 import React from "react"
-import Back from "../components/background"
+import { graphql } from "gatsby"
 
+import Back from "../components/background"
 import Seo from "../components/seo"
 import Layout from "../components/layout"
 
 import "../styles/global.css"
 
-export default function Home() {
-  return (
+export default function Home({ data }) {
+
+	const category = data.microcmsPosts.category.category ? data.microcmsPosts.category.category : "カテゴリなし"
+
+	return (
     <>
       <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
 
@@ -44,37 +48,34 @@ export default function Home() {
             type="text/css"
           />
 
-					<Seo
-						pagetitle="PostPage"
-						pageDescription="これは投稿ページ。次にcreatePageで動的に生成します"
-					/>
+          <Seo
+            pagetitle="PostPage"
+            pageDescription="これは投稿ページ。次にcreatePageで動的に生成します"
+          />
 
           <Back>
-          <header
-            className="masthead">
-            <div className="overlay" />
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-8 col-md-10 mx-auto">
-                  <div className="post-heading">
-                    <h1>
-                      Man must explore, and this is exploration at its greatest
-                    </h1>
-                    <h2 className="subheading">
-                      Problems look mighty small from 150 miles up
-                    </h2>
-                    <span className="meta">
-                      Posted by
-                      <a href="#!">Start Bootstrap</a>
-                      on August 24, 2021
-                    </span>
+            <header className="masthead">
+
+              <div className="overlay" />
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-8 col-md-10 mx-auto">
+                    <div className="post-heading">
+                      <h1>{data.microcmsPosts.title}</h1>
+                      <h2 className="subheading">
+                        {category}
+                      </h2>
+                      <span className="meta">
+                        Posted by
+                        <a href="#!">Start Bootstrap</a>
+                        on {data.microcmsPosts.updatedAtJP}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </header>
-
-					</Back>
+            </header>
+          </Back>
           {/* Post Content*/}
           <article>
             <div className="container">
@@ -182,9 +183,28 @@ export default function Home() {
             </div>
           </article>
           <hr />
-
         </>
       </Layout>
     </>
   )
 }
+
+export const query = graphql`
+query {
+  microcmsPosts(link: {eq: "setup-gatsby"}) {
+    title
+    updatedAtJP: updatedAt(formatString: "")
+    updatedAt
+    link
+    category {
+      category
+      categorySlug
+    }
+  }
+  microcmsCategory {
+    category
+    categorySlug
+  }
+}
+
+`
