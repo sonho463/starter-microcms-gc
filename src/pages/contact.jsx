78175b9c5b2cc90fc00735bf9a9f132d7f2d8230
sonho463 <son.ho463@gmail.com>
2021-05-23 14:43:10 +0900

    とりあえずグーグルフォームで妥協

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -9,7 +9,7 @@ import Layout from "../components/layout"
 import "../styles/global.css"
 
 export default function Contact(location) {
-	const { placeholderImage } = useStaticQuery(
+  const { placeholderImage } = useStaticQuery(
     graphql`
       query {
         placeholderImage: file(relativePath: { eq: "contact-bg.jpg" }) {
@@ -40,7 +40,11 @@ export default function Contact(location) {
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         {/* Font Awesome icons (free version)*/}
 
-        <Seo pageTitle="Contact" pageDescription="これはコンタクトページです" pagepath={location.path} />
+        <Seo
+          pageTitle="Contact"
+          pageDescription="これはコンタクトページです"
+          pagepath={location.path}
+        />
 
         <Layout>
           {/* Page Header*/}
@@ -60,7 +64,9 @@ export default function Contact(location) {
                   <div className="col-lg-8 col-md-10 mx-auto">
                     <div className="page-heading">
                       <h1>Contact Me</h1>
-                      <span className="subheading">Have questions? I have answers.</span>
+                      <span className="subheading">
+                        Have questions? I have answers.
+                      </span>
                     </div>
                   </div>
                 </div>
@@ -72,83 +78,26 @@ export default function Contact(location) {
             {/* Main Content*/}
             <div className="container">
               <div className="row">
-                <div className="col-lg-8 col-md-10 mx-auto">
-                  <p>
-                    Want to get in touch? Fill out the form below to send me a
-                    message and I will get back to you as soon as possible!
-                  </p>
-                  {/* Contact Form - Enter your email address on line 19 of the mail/contact_me.php file to make this form work.*/}
-                  {/* WARNING: Some web hosts do not allow emails to be sent through forms to common mail hosts like Gmail or Yahoo. It's recommended that you use a private domain email address!*/}
-                  {/* To use the contact form, your site must be on a live web host with PHP! The form will not work locally!*/}
-                  <form id="contactForm" name="sentMessage" noValidate>
-                    <div className="control-group">
-                      <div className="form-group floating-label-form-group controls">
-                        <label>Name</label>
-                        <input
-                          className="form-control"
-                          id="name"
-                          type="text"
-                          placeholder="Name"
-                          required
-                          data-validation-required-message="Please enter your name."
-                        />
-                        <p className="help-block text-danger" />
-                      </div>
-                    </div>
-                    <div className="control-group">
-                      <div className="form-group floating-label-form-group controls">
-                        <label>Email Address</label>
-                        <input
-                          className="form-control"
-                          id="email"
-                          type="email"
-                          placeholder="Email Address"
-                          required
-                          data-validation-required-message="Please enter your email address."
-                        />
-                        <p className="help-block text-danger" />
-                      </div>
-                    </div>
-                    <div className="control-group">
-                      <div className="form-group col-xs-12 floating-label-form-group controls">
-                        <label>Phone Number</label>
-                        <input
-                          className="form-control"
-                          id="phone"
-                          type="tel"
-                          placeholder="Phone Number"
-                          required
-                          data-validation-required-message="Please enter your phone number."
-                        />
-                        <p className="help-block text-danger" />
-                      </div>
-                    </div>
-                    <div className="control-group">
-                      <div className="form-group floating-label-form-group controls">
-                        <label>Message</label>
-                        <textarea
-                          className="form-control"
-                          id="message"
-                          rows={5}
-                          placeholder="Message"
-                          required
-                          data-validation-required-message="Please enter a message."
-                          defaultValue={""}
-                        />
-                        <p className="help-block text-danger" />
-                      </div>
-                    </div>
-                    <br />
-                    <div id="success" />
-                    <button
-                      className="btn btn-primary"
-                      id="sendMessageButton"
-                      type="submit"
-                    >
-                      Send
-                    </button>
-                  </form>
+                {/* <div className="col-lg-8 col-md-10 mx-auto"> */}
+                <p>
+                  Want to get in touch? Fill out the form below to send me a
+                  message and I will get back to you as soon as possible!
+                </p>
+
+                <div className="container mx-auto">
+                  <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSdOPYA_XS9Ab7HBl1KcjEk3P3fDDGXj-uU6-CSUrPwPvDg2sw/viewform?embedded=true"
+                    title="form"
+										width="100%"
+                    height="900"
+                    frameborder="0"
+                    marginheight="0"
+                    marginwidth="0"
+                  >
+                    読み込んでいます…
+                  </iframe>
                 </div>
+                {/* </div> */}
               </div>
             </div>
             <hr />
